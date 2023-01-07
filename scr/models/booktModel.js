4ba8657ba8c135ed587b27c355fcbb4a22342e60
Fernando manarim <fcmanarim@gmail.com>
2023-01-07 06:37:41 -0300

    Schema Model Created

---
+++ scr/models/booktModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export BookSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Enter the book name:'
+    },
+    author: {
+        type: String,
+        required: 'Enter the author name:'
+    },
+    sinopse: {
+        type: String,
+        required: 'Enter the sinopse of the book:'
+    },
+    idiom: {
+        type: String,
+        required: 'Enter the idiom:'
+    },
+    isbn: {
+        type: Number,
+        required: 'Enter ISBN:'
+    },
+    date: {
+        type: Number,
+        default : Date.now()
+    }
+});
